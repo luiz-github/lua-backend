089ec39c50b41c7299d69f7c6647a79b159f98e2
Luiz Gustavo Lima Drilard <luizgustavolimadrilard@gmail.com>
2025-08-02 15:59:47 -0300

    FEAT: Add cookieParser middleware

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import productRoutes from "./routes/product.routes"
 import userRoutes from "./routes/user.routes"
 import authRoutes from "./routes/auth.routes"
 
-const express = require('express')
-var cors = require('cors')
 const cookieParser = require('cookie-parser');
+const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use("/product", productRoutes)
 app.use("/user", userRoutes)
